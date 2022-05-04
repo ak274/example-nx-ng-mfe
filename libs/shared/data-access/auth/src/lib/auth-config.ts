5e7920815a6628436d73f6ee44e78f08ffb7863e
Ankush Kalia <ankush.kalia@ihsmarkit.com>
2022-05-04 16:52:59 +0530

    auth config updated

--- libs/shared/data-access/auth/src/lib/auth-config.ts
+++ libs/shared/data-access/auth/src/lib/auth-config.ts
@@ -2,7 +2,7 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
   issuer: 'https://idsvr4.azurewebsites.net',
-  redirectUri: window.location.origin + '/index.html',
+  redirectUri: window.location.origin + '/',
   clientId: 'spa',
   responseType: 'code',
   scope: 'openid profile email offline_access api',
